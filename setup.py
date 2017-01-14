4a29eaa729cac931bfeca81151bc918cecc9ea33
Alejandro Angulo <aab.j13@gmail.com>
2017-01-13 22:48:36 -0800

    bump version number [skip ci]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'django-static-precompiler',
         'mistune',
     ],
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     test_suite='runtests.runtests',
