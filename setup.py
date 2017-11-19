1b40fae876ad9ff8797c4f9feae467dbdae171f3
Alejandro Angulo <aab.j13@gmail.com>
2017-11-18 19:18:12 -0800

    [ci skip] update version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'django-static-precompiler',
         'mistune',
     ],
-    version='0.2',
+    version='0.2.1',
     packages=find_packages(),
     include_package_data=True,
     test_suite='runtests.runtests',
